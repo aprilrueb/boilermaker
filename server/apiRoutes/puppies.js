4ce6b5fb5b6fbfcd0399c0c1c476b931600282fb
April Rueb <aprilrueb@gmail.com>
2017-10-25 14:23:48 -0400

    set up express and react

---
+++ server/apiRoutes/puppies.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+
+router.get('/', function (req, res, next) {
+
+});
+
+router.post('/', function (req, res, next) {
+
+});
+
+router.put('/:puppyId', function (req, res, next) {
+
+});
+
+router.delete('/:puppyId', function (req, res, next) {
+
+});
+
+module.exports = router;
