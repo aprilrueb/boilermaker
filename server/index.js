eac2cbc73714fb7e6a0157c8e3d10742b0647263
April Rueb <aprilrueb@gmail.com>
2017-10-26 12:30:12 -0400

    working on database issue

--- server/index.js
+++ server/index.js
@@ -5,21 +5,20 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 //const port = process.env.PORT || 3000;
 
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, '../public')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/api', require('./apiRoutes'));
-
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../public'));
-});
-
-app.use(function (err, req, res, next) {
-  console.error(err);
-  console.error(err.stack);
-  res.status(err.status || 500).send(err.message || 'Internal server error.');
-});
+module.exports = app
+  .use(morgan('dev'))
+  .use(express.static(path.join(__dirname, '../public')))
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use('/api', require('./apiRoutes'))
+  .get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public'));
+    })
+  .use(function (err, req, res, next) {
+    console.error(err);
+    console.error(err.stack);
+    res.status(err.status || 500).send(err.message || 'Internal server error.');
+    });
 
 // app.listen(port, function () {
 //   console.log('Knock, knock');
@@ -27,3 +26,4 @@ app.use(function (err, req, res, next) {
 //   console.log(`Your server, listening on port ${port}`);
 // });
 
+module.exports = app;
